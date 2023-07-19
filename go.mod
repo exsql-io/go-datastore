14144e865c483f1f7c87a8b726854eec43c6e81b
Benjamin Schaff <benjamin.schaff@gmail.com>
2023-07-18 23:56:22 -0400

    feat: basic sql parser Adding pretty basic mysql sql parser to generate substrate plan.

--- go.mod
+++ go.mod
@@ -12,23 +12,66 @@ require (
 )
 
 require (
+	github.com/DataDog/datadog-agent/pkg/obfuscate v0.42.0 // indirect
+	github.com/DataDog/datadog-agent/pkg/remoteconfig/state v0.42.0 // indirect
+	github.com/DataDog/datadog-go/v5 v5.2.0 // indirect
+	github.com/DataDog/go-tuf v0.3.0--fix-localmeta-fork // indirect
+	github.com/DataDog/sketches-go v1.4.1 // indirect
 	github.com/JohnCGriffin/overflow v0.0.0-20211019200055-46fa312c352c // indirect
+	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/alecthomas/participle/v2 v2.0.0 // indirect
-	github.com/fatih/color v1.13.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/goccy/go-yaml v1.9.8 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/flatbuffers v23.1.21+incompatible // indirect
-	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/google/safehtml v0.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/opentracing-contrib/go-grpc v0.0.0-20210225150812-73cb765af46e // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.17 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.39.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/secure-systems-lab/go-securesystemslib v0.4.0 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.15.0 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/twmb/franz-go/pkg/kmsg v1.4.0 // indirect
+	github.com/uber/jaeger-client-go v2.30.0+incompatible // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go4.org/intern v0.0.0-20220617035311-6925f38cc365 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20220617031537-928513b29760 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/exp v0.0.0-20230206171751-46f607a40771 // indirect
 	golang.org/x/mod v0.8.0 // indirect
@@ -36,6 +79,13 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/genproto v0.0.0-20230209215440-0dfe4f8abfcc // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	gopkg.in/DataDog/dd-trace-go.v1 v1.47.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	inet.af/netaddr v0.0.0-20220811202034-502d2d690317 // indirect
+	vitess.io/vitess v0.17.0 // indirect
 )
--- server.go
+++ server.go
@@ -9,15 +9,20 @@ import (
 	"github.com/apache/arrow/go/v13/arrow/memory"
 	"github.com/exsql-io/go-datastore/services"
 	"github.com/exsql-io/go-datastore/store"
+	"github.com/goccy/go-json"
 	"github.com/labstack/echo/v4"
+	"github.com/substrait-io/substrait-go/expr"
 	"github.com/substrait-io/substrait-go/extensions"
 	"github.com/substrait-io/substrait-go/plan"
 	"github.com/substrait-io/substrait-go/proto"
+	"github.com/substrait-io/substrait-go/types"
+	"golang.org/x/exp/slices"
 	"google.golang.org/protobuf/encoding/protojson"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"vitess.io/vitess/go/vt/sqlparser"
 )
 
 func main() {
@@ -63,6 +68,46 @@ func main() {
 		p, err := plan.FromProto(&planProto, &extensions.DefaultCollection)
 		if err != nil {
 			log.Println(err.Error())
+			return err
+		}
+
+		filter, err := extractFilterFromPlan(p)
+		if err != nil {
+			return err
+		}
+
+		extensionSet := exprs.NewExtensionSet(p.ExtensionRegistry(), exprs.DefaultExtensionIDRegistry)
+		schema := (*leaf.Store).Schema()
+		iterator, err := (*leaf.Store).Iterator(func(input compute.Datum) (compute.Datum, error) {
+			ctx := exprs.WithExtensionIDSet(compute.WithAllocator(context.Background(), memory.DefaultAllocator), extensionSet)
+			return exprs.ExecuteScalarExpression(ctx, schema, filter.Condition(), input)
+		})
+
+		if err != nil {
+			return err
+		}
+
+		defer (*iterator).Close()
+
+		return iteratorResponse(iterator, echoContext)
+	})
+
+	e.POST("/streams/:topic/sql", func(echoContext echo.Context) error {
+		body, err := io.ReadAll(echoContext.Request().Body)
+		if err != nil {
+			log.Println(err.Error())
+			return err
+		}
+
+		var message map[string]string
+		err = json.Unmarshal(body, &message)
+		if err != nil {
+			log.Println(err.Error())
+			return err
+		}
+
+		p, err := planFromSql(leaf.Store, message["query"])
+		if err != nil {
 			return err
 		}
 
@@ -158,4 +203,101 @@ func extractFilterFromPlan(p *plan.Plan) (*plan.FilterRel, error) {
 	log.Printf("%+v\n", rel)
 
 	return nil, errors.New("not implemented")
+}
+
+func planFromSql(s *store.Store, sql string) (*plan.Plan, error) {
+	statement, err := sqlparser.Parse(sql)
+	if err != nil {
+		return nil, err
+	}
+
+	switch stm := statement.(type) {
+	case *sqlparser.Select:
+		switch condition := stm.Where.Expr.(type) {
+		case *sqlparser.ComparisonExpr:
+			builder := plan.NewBuilderDefault()
+			rel, err := buildFilter(s, builder, condition)
+			if err != nil {
+				return nil, err
+			}
+
+			p, err := builder.Plan(rel, toColumnNames(stm.SelectExprs))
+			if err != nil {
+				return nil, err
+			}
+
+			return p, nil
+		}
+	}
+
+	return nil, errors.New("not implemented")
+}
+
+func buildFilter(s *store.Store, builder plan.Builder, comparison *sqlparser.ComparisonExpr) (*plan.FilterRel, error) {
+	scan := builder.NamedScan([]string{"json-events"}, (*s).NamedStruct())
+
+	condition, err := toCondition(builder, scan, comparison)
+	if err != nil {
+		return nil, err
+	}
+
+	filter, err := builder.Filter(scan, condition)
+	if err != nil {
+		return nil, err
+	}
+
+	return filter, nil
+}
+
+func toCondition(builder plan.Builder, scan *plan.NamedTableReadRel, comparison *sqlparser.ComparisonExpr) (*expr.ScalarFunction, error) {
+	left, err := toArg(builder, scan, comparison.Left)
+	if err != nil {
+		return nil, err
+	}
+
+	right, err := toArg(builder, scan, comparison.Right)
+	if err != nil {
+		return nil, err
+	}
+
+	fn, err := builder.ScalarFn(
+		"https://github.com/substrait-io/substrait/blob/main/extensions/functions_comparison.yaml",
+		"equal",
+		nil,
+		left,
+		right,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return fn, nil
+}
+
+func toArg(builder plan.Builder, scan *plan.NamedTableReadRel, sqlExpr sqlparser.Expr) (types.FuncArg, error) {
+	switch value := sqlExpr.(type) {
+	case *sqlparser.Literal:
+		return expr.NewPrimitiveLiteral(value.Val, false), nil
+	case *sqlparser.ColName:
+		index := slices.Index(scan.BaseSchema().Names, value.Name.String())
+		return builder.RootFieldRef(scan, int32(index))
+	}
+
+	return nil, errors.New("not implemented")
+}
+
+func toColumnNames(selectExprs sqlparser.SelectExprs) []string {
+	columns := make([]string, len(selectExprs))
+	for _, e := range selectExprs {
+		switch column := e.(type) {
+		case *sqlparser.Nextval:
+			switch literal := column.Expr.(type) {
+			case *sqlparser.Literal:
+				columns = append(columns, literal.Val)
+			}
+		}
+	}
+
+	return columns
 }
